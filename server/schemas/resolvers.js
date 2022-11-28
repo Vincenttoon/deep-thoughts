08e69327029c1c2b53fccb445a8182c85370f8f1
Vincent Toon <vincenttoon22@gmail.com>
2022-11-28 11:27:16 -0600

    mutation added

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,34 +1,129 @@
+const { AuthenticationError } = require("apollo-server-express");
+const { signToken } = require("../utils/auth");
 const { User, Thought } = require("../models");
 
 const resolvers = {
   Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({})
+          .select("-__v -password")
+          .populate("thoughts")
+          .populate("friends");
+
+        return userData;
+      }
+
+      throw new AuthenticationError("Not logged in");
+    },
+
     thoughts: async (parent, { username }) => {
-        const params = username ? { username } : {};
+      const params = username ? { username } : {};
       return Thought.find(params).sort({ createdAt: -1 });
     },
 
-    thought: async (parent, { _id}) => {
-        return Thought.findOne({ _id });
+    thought: async (parent, { _id }) => {
+      return Thought.findOne({ _id });
     },
 
     users: async () => {
-        return User.find()
-            .select('-__v -password')
-            .populate('friends')
-            .populate('thoughts');
+      return User.find()
+        .select("-__v -password")
+        .populate("friends")
+        .populate("thoughts");
     },
 
     user: async (parent, { username }) => {
-        return User.findOne({ username })
-            .select('-__v -password')
-            .populate('friends')
-            .populate('thoughts');
+      return User.findOne({ username })
+        .select("-__v -password")
+        .populate("friends")
+        .populate("thoughts");
+    },
+  },
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return { token, user };
+    },
+
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+
+    addThought: async (parent, args, context) => {
+      if (context.user) {
+        const thought = await Thought.create({
+          ...args,
+          username: context.user.username,
+        });
+
+        await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $push: { thoughts: thought._id } },
+          { new: true }
+        );
+
+        return thought;
+      }
+
+      throw new AuthenticationError("You need to be logged in!");
+    },
+
+    addReaction: async (parent, { thoughtId, reactionBody }, context) => {
+      if (context.user) {
+        const updatedThought = await Thought.findOneAndUpdate(
+          { _id: thoughtId },
+          {
+            $push: {
+              reactions: { reactionBody, username: context.user.username },
+            },
+          },
+          { new: true, runValidators: true }
+        );
+
+        return updatedThought;
+      }
+
+      throw new AuthenticationError("You need to be logged in!");
+    },
+
+    addFriend: async (parent, { friendId }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { friends: friendId } },
+          { new: true }
+        ).populate("friends");
+
+        return updatedUser;
+      }
+
+      throw new AuthenticationError("You need to be logged in!");
     },
   },
 };
 
 module.exports = resolvers;
 
+// thought id
 // 63842937e1b55f469fc9eaeb
 
+// user id
 //63842937e1b55f469fc9ea8f
+
+// login token : eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7InVzZXJuYW1lIjoidGVzdGVyMiIsImVtYWlsIjoidGVzdDJAdGVzdC5jb20iLCJfaWQiOiI2Mzg0NGEyYzI1YzQ2OTU1MDYwMTg1YmEifSwiaWF0IjoxNjY5NjE1MzkwLCJleHAiOjE2Njk2MjI1OTB9.9KoAmn0PNrFTJOZfqeApnHnN4tR6xhSyLdTdlBYUCek
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -8,7 +8,7 @@ const typeDefs = gql`
     thoughtText: String
     createdAt: String
     username: String
-    reactions: [Reaction]
+    reaction: [Reaction]
   }
 
   type Reaction {
@@ -28,10 +28,24 @@ const typeDefs = gql`
   }
 
   type Query {
+    me: User
     users: [User]
     user(username: String!): User
     thoughts(username: String): [Thought]
     thought(_id: ID!): Thought
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addThought(thoughtText: String!): Thought
+    addReaction(thoughtId: ID!, reactionBody: String!): Thought
+    addFriend(friendId: ID!): User
+  }
+
+  type Auth {
+    token: ID!
+    user: User
   }
 `;
 
